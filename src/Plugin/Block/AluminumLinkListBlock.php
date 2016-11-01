0bd30bb0bb6f6f22627c4f169efe59a06ac25a59
Ben McClure <ben.mcclure@gmail.com>
2016-11-01 00:26:28 -0500

    Add a really simple "Link list" block

---
+++ src/Plugin/Block/AluminumLinkListBlock.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BMcClure
+ * Date: 9/9/2016
+ * Time: 2:13 PM
+ */
+
+namespace Drupal\aluminum_blocks\Plugin\Block;
+
+/**
+ * Provides a 'Copyright information' block
+ *
+ * @Block(
+ *     id = "aluminum_link_list",
+ *     admin_label = @Translation("Link list"),
+ * )
+ */
+class AluminumLinkListBlock extends AluminumBlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getOptions() {
+    return [
+      'link_list' => [
+        '#type' => 'textarea',
+        '#title' => $this->t('Link list'),
+        '#description' => $this->t('Enter your links, one per line, in the format "href|Title|icon".'),
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#theme' => 'aluminum_link_list',
+      '#list' => $this->getList(),
+    ];
+  }
+
+  protected function getList() {
+    $list = [];
+
+    $links = $this->getOptionValue('link_list');
+
+    $links = explode("\n", $links);
+
+    foreach ($links as $linkItem) {
+      $parts = explode("|", $linkItem);
+      $href = array_shift($parts);
+
+      if (!empty($href)) {
+        $list[] = [
+          'href' => $href,
+          'title' => !empty($parts) ? array_shift($parts) : $href,
+          'icon' => !empty($parts) ? array_shift($parts) : '',
+        ];
+      }
+    }
+
+    return $list;
+  }
+}
